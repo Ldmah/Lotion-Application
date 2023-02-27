1dfb7fc6e039dc346887dbe2af4d175287eda8f3
Liam Mah <liam.mah@ucalgary.ca>
2023-02-26 17:23:30 -0700

    Added the base layout.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 function App() {
-  return <h1>Lotion</h1>;
+  return (
+    <>
+      <div id="title-bar">
+        <button id="showNotes">&#9776;</button>
+        <div id="title">
+          <h1>Lotion</h1>
+          <p id="description"><strong>Like Notion, but worse.</strong></p>
+        </div>
+      </div>
+      <div id="content">
+        <div id="noteList">
+          <div>
+            <h3>Notes</h3>
+          </div>
+          <div>
+            <button id="addNew">&#43;</button>
+          </div>
+        </div>
+        <div id="noteContent">
+          Select a note, or create a new one.
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
